3851fe9b765ab706c591acfab7e9b23f2ed789c4
StefannyDef <stefannydefreitas@gmail.com>
2022-05-24 22:18:21 -0400

    commit

---
+++ liking.js
@@ -0,0 +1,11 @@
+
+function extra(){
+    document.querySelector("#extra").innerText++;
+}
+
+function maior(){
+    document.querySelector("#maior").innerText++;
+}
+function lastone(){
+    document.querySelector("#lastone").innerText++;
+}
